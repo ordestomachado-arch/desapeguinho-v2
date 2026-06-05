f34910e56143b8bd474eaabc1610666705661bb1
ordestomachado-arch <ordestomachado@gmail.com>
2026-06-04 22:12:48 -0300

    Update page.tsx

--- app/cadastro/page.tsx
+++ app/cadastro/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { supabase } from '../../supabaseClient' 
 
 export default function CadastroAnuncio() {
