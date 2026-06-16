310b90133f51e78d73bc64612b142db67bc2d083
ordestomachado-arch <ordestomachado@gmail.com>
2026-06-15 21:54:23 -0300

    path supabase corrigido

--- app/meus-anuncios/page.tsx
+++ app/meus-anuncios/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useEffect, useState } from 'react'
-import { supabase } from '../supabaseClient'
+import { supabase } from '../../supabaseClient'
 import Link from 'next/link'
 
 export default function MeusAnuncios() {
